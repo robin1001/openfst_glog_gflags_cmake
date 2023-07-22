592d6e9be2bb00d2405dc5a1de147561d4aeb26f
Binbin Zhang <binbzha@qq.com>
2023-07-22 11:55:56 +0800

    build gflags with shell

---
+++ gflags.sh
@@ -0,0 +1,9 @@
+#!/usr/env/bash
+
+# Since FetchContent has some side effect, so we build it in shell.
+# Download all
+[ ! -f v2.2.2.zip ] && wget -c https://github.com/gflags/gflags/archive/v2.2.2.zip
+pushd gflags-2.2.2
+cmake -B build -DGFLAGS_BUILD_SHARED_LIBS=ON -DGFLAGS_BUILD_STATIC_LIBS=OFF -DGFLAGS_BUILD_gflags_LIB=ON
+cmake --build build
+popd
